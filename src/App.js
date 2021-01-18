a0d413bfcc3b4f9ba2af6a6c2566b8eb0ae49f00
riz1-lv <riz.shkh1@gmail.com>
2021-01-18 16:54:12 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component} from "react";
+//import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+console.log(clientId);
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+    item: {
+      album: {
+        images: [{ url: "" }]
+      },
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
+    },
+    is_playing: "Paused",
+    progress_ms: 0
+  };
+
+  }
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {!this.state.token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+    {/**this.state.token && (
+        // Spotify Player Will Go Here In the Next Step
+    )*/}
+      </header>
+    </div>
+  );
+  }
+}
+export default App;
